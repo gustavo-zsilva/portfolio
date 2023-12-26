f6e6c08656002de71ce142b79a221ef610647bbd
Gustavo Zonta <gustavozontadasilva@gmail.com>
2023-12-26 19:09:20 -0300

    added js to links

---
+++ main.js
@@ -0,0 +1,29 @@
+const linksUl = document.querySelector('.links')
+
+document.addEventListener('DOMContentLoaded', getUserInfo)
+
+async function getUserInfo() {
+    try {
+        const response = await axios.get('https://api.github.com/users/gustavo-zsilva')
+        const { company, html_url, twitter_username, location } = response.data
+
+        showUserInfo({ company, html_url, twitter_username, location })
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+function showUserInfo({ company, html_url, twitter_username, location }) {
+    const locationTag = linksUl.querySelector('.location a')
+    const companyTag = linksUl.querySelector('.company a')
+    const githubTag = linksUl.querySelector('.github a')
+    const twitterTag = linksUl.querySelector('.twitter a')
+
+    locationTag.textContent = location || 'No location specified'
+    companyTag.textContent = company || 'Not working at a company'
+    githubTag.textContent = html_url || 'Couldn\'t retrieve Github URL'
+    twitterTag.textContent = twitter_username || 'No Twitter account specified'
+
+    githubTag.setAttribute('href', html_url)
+    twitterTag.setAttribute('href', `https://twitter.com/${twitter_username}`)
+}
