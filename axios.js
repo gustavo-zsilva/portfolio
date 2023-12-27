aeb7fdfe5fab1e4d063b38f6a875fa41b7f642b7
Gustavo Zonta <gustavozontadasilva@gmail.com>
2023-12-27 12:43:18 -0300

    Finished 'popularRepos' code

---
+++ axios.js
@@ -0,0 +1,5 @@
+const api = axios.create({
+    baseURL: 'https://api.github.com/users/gustavo-zsilva',
+})
+
+export default api
--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+
+
 const linksUl = document.querySelector('.links')
 
 document.addEventListener('DOMContentLoaded', () => {
     getUserInfo()
-    getRecentRepos()
+    getPopularRepos()
 })
 
 async function getUserInfo() {
@@ -33,30 +35,79 @@ function showUserInfo({ company, html_url, twitter_username, location }) {
 
 // Get 2 most recent repos
 
-async function getRecentRepos() {
+async function getPopularRepos() {
     try {
         const response = await axios.get('https://api.github.com/users/gustavo-zsilva/repos?per_page=100')
         const repos = response.data
 
-        const stargazersArr = repos.map(({ stargazers_count }, index) => stargazers_count)
+        const filteredRepos = [{ stars: 0 }, { stars: 0 }]
 
-        let largestNumber = { stargazers_count: 0, index: 0 }
-
-        stargazersArr.forEach((stargazers_count, index) => {
-            if (stargazers_count > largestNumber.stargazers_count) {
-                largestNumber.stargazers_count = stargazers_count
-
-                stargazersArr.splice(index, 1)
-                return
+        repos.forEach(repo => {
+            for (let i = 0; i < 2; i++) {
+                if (repo.stargazers_count > filteredRepos[i].stars) {
+                    filteredRepos[i] = {
+                        name: repo.name,
+                        description: repo.description,
+                        language: repo.language,
+                        stars: repo.stargazers_count,
+                        url: repo.html_url,
+                        getBranches: async () => {
+                            try {
+                                const response = await axios.get(`https://api.github.com/repos/gustavo-zsilva/${repo.name}/branches`)
+                                const numberOfBranches = response.data.length
+                                return numberOfBranches
+                            } catch (err) {
+                                console.error(err)
+                                return 0
+                            }
+                        }
+                    }
+                    return
+                }
             }
-
         })
 
-        console.log(largestNumber);
-
-        // const filteredRepos = repos.filter((repo) => )
-        // console.log(filteredRepos);
+        showPopularRepos(filteredRepos)
     } catch (err) {
         console.error(err)
     }
+}
+
+function showPopularRepos(repos) {
+    const latestProjectsDiv = document.querySelector('.latest-projects')
+
+    repos.forEach(async (repo) => {
+
+        const branches = await repo.getBranches()
+
+        const newRepo = document.createRange().createContextualFragment(`
+        <div class="card project-overview">
+            <header>
+                <img src="./assets/folder.svg" alt="Folder Icon">
+                <a href="${repo.url}" target="_blank" rel="noopener noreferrer">
+                    <h3>${repo.name}</h3>
+                </a>
+            </header>
+            <p>${repo.description}</p>
+            <footer>
+                <p class="stats">
+                    <span>
+                        <img src="./assets/star.svg" alt="Star Icon">
+                        <span>${repo.stars}</span>
+                    </span>
+                    <span>
+                        <img src="./assets/git-branch.svg" alt="Git Branch Icon">
+                        <span>${branches}</span>
+                    </span>
+                </p>
+                <p class="tech">
+                    <span class="circle ${repo.language.toLowerCase()}"></span>
+                    <span>${repo.language}</span>
+                </p>
+            </footer>
+        </div>
+        `)
+
+        latestProjectsDiv.appendChild(newRepo)
+    })
 }
