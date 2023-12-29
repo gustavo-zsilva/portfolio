74924d9aeb0e2d8240df2e70c37fee73f8bee3d7
Gustavo Zonta <gustavozontadasilva@gmail.com>
2023-12-29 13:11:05 -0300

    Added github login

--- modules/userInfo.js
+++ modules/userInfo.js
@@ -6,15 +6,15 @@ import api from '../axios.js'
 export async function getUserInfo() {
     try {
         const response = await api.get('')
-        const { company, html_url, twitter_username, location } = response.data
+        const { login, company, html_url, twitter_username, location } = response.data
 
-        showUserInfo({ company, html_url, twitter_username, location })
+        showUserInfo({ login, company, html_url, twitter_username, location })
     } catch (err) {
         console.error(err)
     }
 }
 
-function showUserInfo({ company, html_url, twitter_username, location }) {
+function showUserInfo({ login, company, html_url, twitter_username, location }) {
     const locationTag = linksUl.querySelector('.location a')
     const companyTag = linksUl.querySelector('.company a')
     const githubTag = linksUl.querySelector('.github a')
@@ -22,7 +22,7 @@ function showUserInfo({ company, html_url, twitter_username, location }) {
 
     locationTag.textContent = location || 'No location specified'
     companyTag.textContent = company || 'Not working at a company'
-    githubTag.textContent = html_url || 'Couldn\'t retrieve Github URL'
+    githubTag.textContent = login || 'Couldn\'t retrieve Github URL'
     twitterTag.textContent = twitter_username || 'No Twitter account specified'
 
     githubTag.setAttribute('href', html_url)
